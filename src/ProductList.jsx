8bd703fcfc4275fe9a5ca008a3750a56c5ea3e48
Nxlfe2003@ <omar.ignammas@uit.ac.ma>
2024-07-22 14:04:37 +0100

    Hello

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,47 @@
+import { useEffect, useState } from "react"
+import Product from "./product"
 
 export default function ProductList(){
+
+  const [products,setproducts] = useState([])
+  const getproducts = () => {
+       fetch('https://fakestoreapi.com/products')
+       .then(Response => Response.json())
+       .then(Response => setproducts(Response))
+
+
+  }
+  useEffect(()=> {
+    getproducts()
+  },[])
+
+  const displayproducts =() => {
+    
+    return products.map((product,key) => {
+      return (<Product product={product} key={key}/>)
+    
+    })
+}
   return (
-    <h1>Liste des produits :</h1>
+    <div className="container-fluix mx-auto w-75 my-3">
+          <h1>Liste des produits :</h1>
+          <table className="table">
+    <thead>
+        <tr>
+            <th>#ID</th>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Image</th>
+            <th>Rating</th>
+        </tr>
+    </thead>
+    <tbody>
+           {displayproducts()}
+    </tbody>
+ </table>
+    </div>
+
   )
 }
---
+++ src/product.jsx
@@ -0,0 +1,15 @@
+export default function Product({product}){
+
+return (
+    <tr>
+    <td>{product.id}</td>
+    <td>{product.title}</td>
+    <td>{product.description}</td>
+    <td>{product.category}</td>
+    <td>{product.image}</td>
+    <td>{product.rating}</td>
+  </tr>
+
+)
+  
+}
